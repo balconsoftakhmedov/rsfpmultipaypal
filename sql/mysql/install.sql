cb9c3ef530e3671e0914686b680e88de71744f92
BK <akhmedov@balcomsoft.com>
2023-08-12 14:25:51 +0500

    ss

--- sql/mysql/install.sql
+++ sql/mysql/install.sql
@@ -16,5 +16,6 @@ DELETE FROM `#__rsform_component_type_fields` WHERE ComponentTypeId = 520;
 INSERT IGNORE INTO `#__rsform_component_type_fields` (`ComponentTypeId`, `FieldName`, `FieldType`, `FieldValues`, `Ordering`) VALUES
 (520, 'NAME', 'textbox', '', 0),
 (520, 'LABEL', 'textbox', '', 1),
+(520, 'Email', 'textbox', '', 1),
 (520, 'COMPONENTTYPE', 'hidden', '520', 2),
 (520, 'LAYOUTHIDDEN', 'hiddenparam', 'YES', 7);
